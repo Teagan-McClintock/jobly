a84f8c4db99277df6cd61eb8a7f8b0c0347e8ed2
Victor Kim <vskimjr@gmail.com>
2024-01-10 15:31:07 -0500

    finished tests for sql.test.js, started writing tests for filtering for companies get / route

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,16 +1,29 @@
 "use strict";
 
+const { BadRequestError } = require("../expressError");
 const { sqlForPartialUpdate } = require("./sql");
 
-describe("sqlForPartialUpdate", function(){
-  test("Should work", function(){
-    const testData = {"test": 5, "test2": 6};
-    const testJsToSql = {"test": "test_column"};
+describe("sqlForPartialUpdate", function () {
+  test("Should work", function () {
+    const testData = { "test": 5, "test2": 6 };
+    const testJsToSql = { "test": "test_column" };
     const sqlOutput = sqlForPartialUpdate(testData, testJsToSql);
 
     expect(sqlOutput).toEqual({
-      setCols:"\"test_column\"=$1, \"test2\"=$2",
+      setCols: "\"test_column\"=$1, \"test2\"=$2",
       values: [5, 6]
     });
+  });
+
+  test("Should not work", function () {
+    const badTestData = {};
+    const testJsToSql = { "test": "test_column" };
+
+    try {
+      sqlForPartialUpdate(badTestData, testJsToSql);
+      throw new Error("fail test, you shouldn't get here");
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
   });
 });
